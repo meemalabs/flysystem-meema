6082edcec5f7775f500548960f403731c6b70090
glennmichael123 <gtorregosa@gmail.com>
2021-04-23 06:51:39 +0800

    Master (#10)
    * initial functions
    
    * Apply fixes from StyleCI (#6)
    
    * Use SDK for API
    
    * Apply fixes from StyleCI (#7)
    
    * use sdk
    
    * Apply fixes from StyleCI (#8)
    
    * finalize
    
    * Apply fixes from StyleCI (#9)
    
    * fix conflict

---
+++ src/MeemaAdapter.php
@@ -0,0 +1,261 @@
+<?php
+
+namespace Meema\Flysystem;
+
+use League\Flysystem\Adapter\AbstractAdapter;
+use League\Flysystem\Config;
+use Meema\MeemaApi\Client;
+
+class MeemaAdapter extends AbstractAdapter
+{
+    /**
+     * @var \Meema\Flysystem\Client
+     */
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new Client(env('MEEMA_API_KEY'));
+    }
+
+    /**
+     * Write a new file.
+     *
+     * @param string $path
+     * @param string $contents
+     * @param Config $config   Config object
+     *
+     * @return false|array false on failure file meta data on success
+     */
+    public function write($path, $contents, Config $config)
+    {
+        return $this->client->storage()->upload($path);
+    }
+
+    /**
+     * Write a new file using a stream.
+     *
+     * @param string   $path
+     * @param resource $resource
+     * @param Config   $config   Config object
+     *
+     * @return array|false false on failure file meta data on success
+     */
+    public function writeStream($path, $resource, Config $config)
+    {
+        return $this->client->storage()->upload($path);
+    }
+
+    /**
+     * Update a file.
+     *
+     * @param string $path
+     * @param string $contents
+     * @param Config $config   Config object
+     *
+     * @return array|false false on failure file meta data on success
+     */
+    public function update($path, $contents, Config $config)
+    {
+        return $this->client->storage()->upload($path);
+    }
+
+    /**
+     * Update a file using a stream.
+     *
+     * @param string   $path
+     * @param resource $resource
+     * @param Config   $config   Config object
+     *
+     * @return array|false false on failure file meta data on success
+     */
+    public function updateStream($path, $resource, Config $config)
+    {
+        return $this->client->storage()->upload($path);
+    }
+
+    /**
+     * Rename a file.
+     *
+     * @param string $path
+     * @param string $newpath
+     *
+     * @return bool
+     */
+    public function rename($path, $newpath)
+    {
+        return $this->client->storage()->rename($path, $newpath);
+    }
+
+    /**
+     * Copy a file.
+     *
+     * @param string $path
+     * @param string $newpath
+     *
+     * @return bool
+     */
+    public function copy($path, $newpath)
+    {
+        return $this->client->storage()->copy($path, $newpath);
+    }
+
+    /**
+     * Delete a file.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function delete($path)
+    {
+        return $this->client->storage()->delete($path);
+    }
+
+    /**
+     * Delete a directory.
+     *
+     * @param string $dirname
+     *
+     * @return bool
+     */
+    public function deleteDir($dirname)
+    {
+        return $this->client->storage()->deleteDirectory($dirname);
+    }
+
+    /**
+     * Create a directory.
+     *
+     * @param string $dirname directory name
+     * @param Config $config
+     *
+     * @return array|false
+     */
+    public function createDir($dirname, Config $config)
+    {
+        return $this->client->storage()->makeDirectory($dirname);
+    }
+
+    /**
+     * Set the visibility for a file.
+     *
+     * @param string $path
+     * @param string $visibility
+     *
+     * @return array|false file meta data
+     */
+    public function setVisibility($path, $visibility)
+    {
+        return $this->client->storage()->setVisibility($path, $visibility);
+    }
+
+    /**
+     * Check whether a file exists.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function has($path)
+    {
+        return $this->client->storage()->has($path);
+    }
+
+    /**
+     * Read a file.
+     *
+     * @param string $path
+     *
+     * @return false|array
+     */
+    public function read($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * List contents of a directory.
+     *
+     * @param string $directory
+     * @param bool   $recursive
+     *
+     * @return array
+     */
+    public function listContents($directory = '', $recursive = false)
+    {
+        return $this->client->storage()->listContents($directory, $recursive);
+    }
+
+    /**
+     * Read a file as a stream.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function readStream($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * Get all the meta data of a file or directory.
+     *
+     * @param string $path
+     *
+     * @return false|array
+     */
+    public function getMetadata($path)
+    {
+        return $this->client->storage()->getMetadata($path);
+    }
+
+    /**
+     * Get all the meta data of a file or directory.
+     *
+     * @param string $path
+     *
+     * @return false|array
+     */
+    public function getSize($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * Get the mimetype of a file.
+     *
+     * @param string $path
+     *
+     * @return false|array
+     */
+    public function getMimetype($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * Get the timestamp of a file.
+     *
+     * @param string $path
+     *
+     * @return false|array
+     */
+    public function getTimestamp($path)
+    {
+        return $this->getMetadata($path);
+    }
+
+    /**
+     * Get the visibility of a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getVisibility($path)
+    {
+        return $this->getMetadata($path);
+    }
+}
